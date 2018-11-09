96d2cf3ab6b88e66f54ec3ae7f81a5e8d7f9fb29
Jeon <wk0012345@gmail.com>
2018-11-09 16:21:24 +0900

    append bithumb crawling

--- crawlingBithumb.py
+++ crawlingBithumb.py
@@ -3,24 +3,24 @@ import json
 import datetime
 import time
 
-class crawlingCoincheck:
+class crawlingBithumb:
 
     http = urllib.PoolManager()
 
     #defind of URL
-    url = "https://coincheck.com/api/ticker.json?pair=btc_jpy"
+    url = "https://api.bithumb.com/public/ticker/BTC"
 
     #defind of exchange
-    exchange = "coincheck"
+    exchange = "bithumb"
 
     #defind of Key
-    apiPriceKey = 'last'
+    apiPriceKey = ['data','closing_price']
 
     def getPrice(self):
 
         ts = time.time()
         st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
-   
+  
         urllib.disable_warnings()
         response = self.http.request('Get',self.url)
 
@@ -31,7 +31,7 @@ class crawlingCoincheck:
         data = response.data.decode('utf-8')
         data = json.loads(data)
 
-        priceData = data[self.apiPriceKey]
+        priceData = data[self.apiPriceKey[0]][self.apiPriceKey[1]]
 
         #Api check
         if priceData is None:
--- runningCrawling.py
+++ runningCrawling.py
@@ -1,8 +1,33 @@
 import crawlingCoincheck
+import crawlingBithumb
 import insert
+import sys
 
-run = crawlingCoincheck.crawlingCoincheck()
+def serverDownCheck(arg):
+    if(arg is False):
+        sys.exit(1)
+
+#defind of dataStore
+dataStore = []
+
+#defind of Database instance
 insertRun = insert.InsertMongoDb()
 
-data = run.GetPrice()
-insertRun.insertData(data)
+'''
+instancing, get price , and append in datastore
+duck typing
+'''
+
+#get coincheck of Bitcoin price
+run = crawlingCoincheck.crawlingCoincheck()
+dataStore.append(run.getPrice())
+
+#get bithumb of Bitcoin price
+run = crawlingBithumb.crawlingBithumb()
+dataStore.append(run.getPrice())
+
+#server down check
+serverDownCheck(run)
+
+#insert into database
+insertRun.insertData(dataStore)
